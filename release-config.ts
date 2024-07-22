156d753eb3e36ddd2483775ee77e556e1b8e93cf
Frederik <frederikbroth@gmail.com>
2024-07-23 01:28:06 +0200

    working??

--- release-config.ts
+++ release-config.ts
@@ -1,6 +1,7 @@
 export default {
   afterRelease: async ({ exec, nextVersion }) => {
     await exec(`echo "${nextVersion}" > release.txt`);
+    
   },
   beforePrepare: async ({ exec, nextVersion }) => {
 
