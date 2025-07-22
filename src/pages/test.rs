50478480d834a14cdf3615cd1fd4b34f98bfe98b
FrederikBRoth <32262672+FrederikBRoth@users.noreply.github.com>
2025-07-22 23:14:10 +0200

    New fix (#125)
    * Specify config file
    
    * PLease
    
    * Updated game wasm files

--- src/pages/test.rs
+++ src/pages/test.rs
@@ -6,7 +6,7 @@ use leptos::prelude::*;
 use wasm_bindgen::prelude::*;
 use web_sys::{js_sys, File, FileList, FileReader, HtmlInputElement, Url};
 
-#[wasm_bindgen(module = "/src/wasm/gameimport.js")]
+#[wasm_bindgen(module = "/public/gameimport.js")]
 extern "C" {
     #[wasm_bindgen(catch)]
     fn initGame() -> Result<JsValue, JsValue>;
@@ -18,5 +18,5 @@ pub fn Game() -> impl IntoView {
         initGame();
     });
 
-    view! { <div id="wasm-example" class="bg-sky-300"></div> }
+    view! { <div id="wasm-example" class="bg-sky-300"><canvas id="canvas" class="bg-sky-300"></canvas></div> }
 }
