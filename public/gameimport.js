8becea53f914c3699a32cff65c25f6765fb9f6f6
Frederik Bøttger-Roth <fred@DESKTOP-NSC7GVA.localdomain>
2025-10-19 11:35:24 +0200

    merge

--- public/gameimport.js
+++ public/gameimport.js
@@ -1,8 +1,8 @@
 import init from "/games/pkg/cv_game.js";
 
 export const initGame = () => {
-  console.log("function called");
+  console.log("function called")
   init().then(() => {
     console.log("WASM Loaded");
   });
-};
+}; 
